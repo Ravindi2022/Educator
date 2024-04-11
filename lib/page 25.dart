e9bae7851cbebc929416457c0902c3ade95815df
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2024-04-11 11:55:00 +0530

    Create page 25.dart

---
+++ lib/page 25.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+
+class page25 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<String> links = [
+    'reference link 1',
+    'reference link 2', // Added a space after "link"
+    'reference link 3',
+    'reference link 4',
+    'reference link 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Algorithms and Data Structure'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'References',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                ...links
+                    .map(
+                      (link) => Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            side: MaterialStateProperty.all(
+                              BorderSide(color: Colors.green),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Handle link tap here
+                          },
+                          child: Text(
+                            link,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
