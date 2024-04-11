54fa9f2c5f7cb7b8528c5fa908e1304119b00d4e
Yashmi-19 <150003382+Yashmi-19@users.noreply.github.com>
2024-04-11 15:47:31 +0530

    Create page 18.dart

---
+++ lib/page 18.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class page18 extends StatelessWidget {
+  const page18({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "educator",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            'Educator',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF0B2348),
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        
+        body: SingleChildScrollView(
+          
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const Text(
+              'Mathematics for Computing',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+                const SizedBox(height: 20), 
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                 const Text('short note 1',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), 
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                 const Text('short note 2',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), 
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                 const Text('short note 3',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), // Adjust spacing as needed
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                 const Text('short note 4',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), // Adjust spacing as needed
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                const Text('short note 5',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), // Adjust spacing as needed
+                ImageButton(
+                  onPressed: () {},
+                  image: const AssetImage('assets/freestock_574456060.jpg'),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                 const Text('short note 6',
+                style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
+                const SizedBox(height: 20), // Adjust spacing as needed
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final ImageProvider image;
+  final double width;
+  final double height;
+
+  const ImageButton({
+    required this.onPressed,
+    required this.image,
+    this.width = 100.0,
+    this.height = 50.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Material(
+        child: InkWell(
+          onTap: onPressed,
+          child: Image(
+            image: image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
