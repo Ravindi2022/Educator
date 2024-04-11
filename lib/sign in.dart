7246641ba2d1eb5a3f43eb1310ea7e97334cb87b
Nadil Lokuhetty <121077619+Nadil2022@users.noreply.github.com>
2024-04-11 12:15:34 +0530

    Create sign in.dart

---
+++ lib/sign in.dart
@@ -0,0 +1,110 @@
+import 'package:educator/home.dart';
+import 'package:educator/page10.dart';
+import 'package:educator/page5.dart';
+import 'package:flutter/material.dart';
+
+class sign_in extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Educator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Educator'),
+      ),
+      body: SingleChildScrollView(
+        // Wrap the body in a SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign in',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email or User Name',
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: TextButton(
+                    onPressed: () {
+                      // Implement password recovery logic
+                    },
+                    child: Text('Forget Password?'),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => home()),
+                        );
+                  // Implement sign-in logic
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xFF50C878)), // Paris Green background color
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      Colors.white), // Text color
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      Size(390, 50)), // Button size
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.zero), // Remove default padding
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(15), // Button border radius
+                      side: BorderSide(
+                        color: Color(0x00000003), // Border color
+                        width: 1, // Border width
+                      ),
+                    ),
+                  ),
+                ),
+                child: Text('Sign in'),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => home()),
+                  );
+                  // Navigate to sign-up page
+                },
+                child: Text("Don't have an account? Sign Up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
