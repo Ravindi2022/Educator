0e755963d83e8daf056c28c68939e56a0e80f52b
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2024-04-12 10:04:24 +0530

    Update page 30.dart

--- lib/page 30.dart
+++ lib/page 30.dart
@@ -5,7 +5,7 @@ class page30 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
