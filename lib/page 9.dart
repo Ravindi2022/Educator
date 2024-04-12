57262f5dcb12b32638aed4625bf0b6d501ee93b2
Yashmi-19 <150003382+Yashmi-19@users.noreply.github.com>
2024-04-12 15:35:36 +0530

    page 9.dart

--- lib/page 9.dart
+++ lib/page 9.dart
@@ -1,91 +1,175 @@
+import 'package:educator/pdfView.dart';
 import 'package:flutter/material.dart';
 
 class page9 extends StatelessWidget {
   const page9({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
-     return MaterialApp(
+    return MaterialApp(
       title: "educator",
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-       
         body: SingleChildScrollView(
           child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                 const Text(
+                const Text(
                   'Introduction to Computer Science',
-                  textAlign: TextAlign.center, style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                 ),
-                 const SizedBox(height: 20,width: 30,), 
-                 
-                 
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/lesson.jpg'),
-                      width: 300.0,
-                      height: 165.0,
-                    ),
-                     const Text('lesson 1',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20), 
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/lesson.jpg'),
-                      width: 300.0,
-                      height: 165.0,
-                    ),
-                     const Text('lesson 2',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20), 
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/lesson.jpg'),
-                      width: 300.0,
-                      height: 165.0,
-                    ),
-                     const Text('lesson 3',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20),
-                 
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/lesson.jpg'),
-                      width: 300.0,
-                      height: 165.0,
-                    ),
-                     const Text('lesson 4',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20), // Adjust spacing as needed
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/lesson.jpg'),
-                      width: 300.0,
-                      height: 165.0,
-                    ),
-                    const Text('lesson 5',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20), // Adjust spacing as needed
-                    ImageButton(
-                      onPressed: () {},
-                      image: const AssetImage('assets/final_paper.jpg'),
-                      width: 300.0,
-                      height: 200.0,
-                    ),
-                     const Text('final paper',
-                    style: TextStyle(fontSize: 18.0, fontWeight:FontWeight.w200),),
-                    const SizedBox(height: 20), 
-            
+                const SizedBox(
+                  height: 20,
+                  width: 30,
+                ),
+
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20questions.pdf?alt=media&token=afef28ec-0633-4a3f-8bd8-6669da8914d6')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/lesson.jpg'),
+                  width: 300.0,
+                  height: 165.0,
+                ),
+                const Text(
+                  'lesson 1',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20),
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20questions.pdf?alt=media&token=afef28ec-0633-4a3f-8bd8-6669da8914d6')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/lesson.jpg'),
+                  width: 300.0,
+                  height: 165.0,
+                ),
+                const Text(
+                  'lesson 2',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20),
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20lesson.pdf?alt=media&token=2987a863-1a51-4037-995e-259e60381a88')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/lesson.jpg'),
+                  width: 300.0,
+                  height: 165.0,
+                ),
+                const Text(
+                  'lesson 3',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20),
+
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20questions.pdf?alt=media&token=afef28ec-0633-4a3f-8bd8-6669da8914d6')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/lesson.jpg'),
+                  width: 300.0,
+                  height: 165.0,
+                ),
+                const Text(
+                  'lesson 4',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20), // Adjust spacing as needed
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20questions.pdf?alt=media&token=afef28ec-0633-4a3f-8bd8-6669da8914d6')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/lesson.jpg'),
+                  width: 300.0,
+                  height: 165.0,
+                ),
+                const Text(
+                  'lesson 5',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20), // Adjust spacing as needed
+                ImageButton(
+                  onPressed: () {
+                    createFileOfPdfUrl(
+                            'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Example%20for%20questions.pdf?alt=media&token=afef28ec-0633-4a3f-8bd8-6669da8914d6')
+                        .then((f) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PDFScreen(Path: f.path)),
+                      );
+                    });
+                  },
+                  image: const AssetImage('assets/final_paper.jpg'),
+                  width: 300.0,
+                  height: 200.0,
+                ),
+                const Text(
+                  'final paper',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 20),
               ],
             ),
           ),
         ),
       ),
-     );
+    );
   }
 }
+
+class PdfViewerPage extends StatelessWidget {
+  final String pdfUrl;
+
+  PdfViewerPage(this.pdfUrl);
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
 class ImageButton extends StatelessWidget {
   final VoidCallback onPressed;
   final ImageProvider image;
@@ -116,3 +200,4 @@ class ImageButton extends StatelessWidget {
     );
   }
 }
+
