9fb8a0fc1225f61f49075168832c9ee0aeafa351
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2024-04-12 15:21:57 +0530

    Update page 11.dart

--- lib/page 11.dart
+++ lib/page 11.dart
@@ -1,93 +1,129 @@
-import 'package:educator/pdfView.dart';
+import 'package:educator/page12.dart';
+import 'package:educator/page13.dart';
+import 'package:educator/page14.dart';
+import 'package:educator/page15.dart';
+import 'package:educator/page8.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart'; // Import url_launcher package
 
-class page10 extends StatelessWidget {
-  @override 
+class page11 extends StatelessWidget {
+const page11({super.key});
+
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final List<List<String>> links = [
-    [
-      'reference link 1',
-      'https://stackoverflow.com/questions/43583411/how-to-create-a-hyperlink-in-flutter-widget'
-    ],
-    ['reference link 2', 'https://example.com/link2'],
-    ['reference link 3', 'https://example.com/link3'],
-    ['reference link 4', 'https://example.com/link4'],
-    ['reference link 5', 'https://example.com/link5'],
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  child: Text(
-                    'Introduction to Computer Science',
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                  ),
+      title: "Educator",
+      home: Stack(
+        children: [
+           
+          Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Text(
+                'Educator',
+                style: TextStyle(
+                  color: Color(0xFF0B2348),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Text(
-                    'References',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Text(
+                    'Programming in C',
+                    style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                   ),
-                ),
-                ...links
-                    .map(
-                      (link) => Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: OutlinedButton(
-                          style: ButtonStyle(
-                            side: MaterialStateProperty.all(
-                              BorderSide(color: Colors.green),
-                            ),
-                          ),
-                          onPressed: () {
-                            createFileOfPdfUrl(
-                                    'https://firebasestorage.googleapis.com/v0/b/educator99-33923.appspot.com/o/Reference%20Links.pdf?alt=media&token=1a8280ce-dfae-462f-9f8a-2c4d6e27e799')
-                                .then((f) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        PDFScreen(Path: f.path)),
-                              );
-                            });
-                            // Launch URL on button press
-                          },
-                          child: Text(
-                            link[0],
-                            style: TextStyle(fontSize: 18),
-                          ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => page12()),
+                      );
+                    },
+                    child: Center(
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF73C67B),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          'Description',
+                          style: TextStyle(color: Colors.black, fontSize: 18.0),
                         ),
                       ),
-                    )
-                    .toList(),
-              ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => page13()),
+                      );
+                    },
+                    child: Center(
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF73C67B),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          'Short Notes',
+                          style: TextStyle(color: Colors.black, fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => page14()),
+                      );},
+                    child: Center(
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF73C67B),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          'Questions',
+                          style: TextStyle(color: Colors.black, fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Page15()),
+                      );},
+                    child: Center(
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF73C67B),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          'References',
+                          style: TextStyle(color: Colors.black, fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
